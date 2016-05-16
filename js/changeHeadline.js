d0dd930d97ecfd020d95436cd9db3ab8d98bf8c4
evilbaschdi <sebwalter@gmx.net>
2016-05-16 12:00:30 +0200

    init

---
+++ js/changeHeadline.js
@@ -0,0 +1,12 @@
+﻿var text = ['//sebastian walter', 'evil.baschdi'];
+var counter = 0;
+var elem = document.getElementById('changeHeadline');
+setInterval(change, 6000);
+
+function change() {
+  elem.innerHTML = text[counter];  
+  counter++;
+  if (counter >= text.length) {
+    counter = 0;
+  }
+}
---
+++ js/changeTitle.js
@@ -0,0 +1,13 @@
+﻿var titles = ['//sebastian walter', 'evil.baschdi'];
+
+function changeTitle() {
+  var millis = 6000,
+      count = 0;
+  titles.push(document.title);
+  setInterval(
+    function() {
+      document.title = titles[count % titles.length];
+      ++count;
+    },
+    millis);
+};
