afe5647547ae10dd7d3bfe81a3c75096d89f2a91
Sebastian Walter <sebwalter@gmx.net>
2019-11-30 10:11:59 +0100

    .

--- bootstrap/js/disclaimer.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("disclaimer");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
